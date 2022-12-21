fac3616f1f3c1a2adbeec09c6056b0156a03ba62
B-asile <84376166+B-asile@users.noreply.github.com>
2022-12-21 09:33:52 +0100

    factorisation

--- authentication/models.py
+++ authentication/models.py
@@ -1,23 +1,5 @@
-from django.contrib.auth.models import AbstractUser, Group
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    CREATOR = 'CREATOR'
-    SUBSCRIBER = 'SUBSCRIBER'
-
-    ROLE_CHOICES = (
-        (CREATOR, 'Créateur'),
-        (SUBSCRIBER, 'Abonné'),
-    )
-
-    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='rôle')
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        if self.role == self.CREATOR:
-            group = Group.objects.get(name='creators')
-            group.user_set.add(self)
-        elif self.role == self.SUBSCRIBER:
-            group = Group.objects.get(name='subscribers')
-            group.user_set.add(self)
-
+    pass
