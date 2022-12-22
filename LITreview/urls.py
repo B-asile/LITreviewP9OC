128584f030ac18dad57bb63e2b1123b801a92189
B-asile <84376166+B-asile@users.noreply.github.com>
2022-12-22 10:45:01 +0100

    read

--- LITreview/urls.py
+++ LITreview/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
-
+'''path urls to different applications'''
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("authentication.urls")),
